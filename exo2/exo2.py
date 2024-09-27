deab1928254ae9e9d8c88885f4fa038f09764f84
landry-01 <biaouadebayo2@gmail.com>
2024-09-27 10:54:17 +0200

    Ex

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,45 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+import unittest
+
+# Fonction à tester
+def solution(string, ending):
+    return string.endswith(ending)
+
+solution('abc', 'bc')
+solution('abc', 'd')
+
+# Classe de tests
+class TestSolution(unittest.TestCase):
+    
+    # Méthode pour le premier use case : vrai lorsque la chaîne se termine par l'ending
+    def test_solution_true_cases(self):
+        fixed_tests_True = [
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails")
+        ]
+        for string, ending in fixed_tests_True:
+            with self.subTest(string=string, ending=ending):
+                self.assertTrue(solution(string, ending))
+    
+    # Méthode pour le second use case : faux lorsque la chaîne ne se termine pas par l'ending
+    def test_solution_false_cases(self):
+        fixed_tests_False = [
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs")
+        ]
+        for string, ending in fixed_tests_False:
+            with self.subTest(string=string, ending=ending):
+                self.assertFalse(solution(string, ending))
+
+if __name__ == '__main__':
+    unittest.main()
